f8b382b04342e0e4a806e1a723944dfa2184a5a6
Sellieee <Sellieee@hotmail.com>
2019-10-13 17:53:20 +1100

    added npms and required our models

---
+++ server.js
@@ -0,0 +1,26 @@
+// Requiring express
+var express = require("express");
+
+// Requiring body-parser
+var bodyParser = require("body-parser");
+
+// Requiring mongoose
+var mongoose = require("mongoose");
+
+// Requiring morgan Middleware to help generate server logs
+var morgan = require("morgan");
+
+// Require path
+var path = require("path");
+
+// Using note.js
+var note = require("./models/note.js");
+
+// Using article.js
+var note = require("./models/article.js");
+
+// Requiring requests
+var request = require("request");
+
+// Requiring Cheerio
+var cheerio = require("cheerio");
