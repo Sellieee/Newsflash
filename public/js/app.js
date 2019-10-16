47befd6e1ff97443a08b5ec232d42eef018d8599
Sellieee <Sellieee@hotmail.com>
2019-10-16 21:21:28 +1100

    added deleting the article and saving notes

--- public/js/app.js
+++ public/js/app.js
@@ -14,10 +14,39 @@ $(".save").on("click", function () {
    var thisId = $(this).attr("data-id");
    $.ajax({
       method: "POST",
-      url: "/articles/save" + thisId
+      url: "/articles/save/" + thisId
    }).done(function (data, res) {
       console.log(data);
       res.redirect("/");
+   });
+});
+
+// Delete the article
+$(".delete").on("click", function () {
+   var thisId = $(this).attr("data-id");
+   $.ajax({
+      method: "POST",
+      url: "/articles/delete/" + thisId
+   }).done(function (data, res) {
+      console.log(data);
+      res.redirect("/saved");
+   });
+});
+
+// Save the note
+$(".save-note").on("click", function () {
+   var thisId = $(this).attr("data-id");
+   $.ajax({
+      method: "POST",
+      url: "/notes/save/" + thisId,
+      data: {
+         text: $("#note-body" + thisId).val()
+      }
+   }).done(function (data, res) {
+      console.log(data);
+      $("#note-body" + thisId).val("");
+      $(".modalNote").modal("hide");
+      res.redirect("/saved");
    });
 });
 
